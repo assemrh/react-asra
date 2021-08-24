eb7d1875bc55e8e12b15b9b39fad74392d975946
asra <assem.rh@outlook.com>
2021-08-24 22:16:27 +0300

    up

--- src/Loading.js
+++ src/Loading.js
@@ -8,7 +8,7 @@ function Loading() {
     return (
         <div  className="App App-header-coomig">
             <ComingSoon
-                style={{margin: '200px', padding: '200px'}}
+                style={{marginTop: '200px', paddingTop: '200px'}}
                 bgColor="#282c34"
                 textColor="#000"
                 >
